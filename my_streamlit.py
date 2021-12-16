8f7aa35a4865a71af6d55c1d87b880a117fad035
cristiantutus <96250833+cristiantutus@users.noreply.github.com>
2021-12-16 22:47:38 +0700

    Update my_streamlit.py

--- my_streamlit.py
+++ my_streamlit.py
@@ -26,15 +26,16 @@ elif option == 'Jumlah Produksi Minyak':
     st.write("""## Jumlah Produksi Minyak""") #menampilkan judul halaman 
 
     txt_negara = st.text_input('Masukan Nama Negara :')
+    dm = pd.read_csv('data/produksi_minyak_mentah.csv')
+    df_js = pd.read_json('data/kode_negara_lengkap.json')
 
     if txt_negara:
         for negara in data_n:
             if negara['name'] == str(txt_negara):
+                # Untuk jawaban 1
                 st.text("Data Negara Yang Di Tunjukan : ")
                 st.text("Kode Negara : " + negara['alpha-3'])
 
-                dm = pd.read_csv('data/produksi_minyak_mentah.csv')
-                
                 data_produksi = dm[dm["kode_negara"].isin([negara['alpha-3']])]["produksi"].tolist()
 
                 st.text("Chart Untuk Kode Negara => {}".format(negara['alpha-3']))
@@ -46,10 +47,10 @@ elif option == 'Jumlah Produksi Minyak':
                 st.line_chart(chart_data)
                 chart_data
                 
+                # Untuk jawaban 1 b
                 data_tahun = dm[dm["kode_negara"].isin([negara['alpha-3']])]["tahun"].tolist()
 
                 in_tahun = st.selectbox('Masukan Tahun ', data_tahun)
-
                 st.text("Chart Pertahun Untuk Kode Negara => {} Pada Tahun {}".format(negara['alpha-3'], in_tahun))
 
                 d_tahun = dm[dm["tahun"].isin([in_tahun])]["produksi"].tolist()
@@ -62,6 +63,7 @@ elif option == 'Jumlah Produksi Minyak':
                 st.line_chart(chart_tahunan)
                 chart_tahunan
 
+                # Untuk jawaban 1 c
                 st.text("Chart Pertumbuhan Untuk Kode Negara => {} Pada Tahun {}".format(negara['alpha-3'], in_tahun))
                 d_pertum = dm[dm["kode_negara"].isin([negara['alpha-3']])]["produksi"].tolist()                
 
@@ -75,16 +77,115 @@ elif option == 'Jumlah Produksi Minyak':
                 st.line_chart(sort_pertum)
                 sort_pertum
 
-                st.write("Data Lengkap Negara :")
+                # Untuk jawaban 1 d
+                # st.write("Data Lengkap Negara :")
 
-                st.text("Nama Negara : {}".format(negara['name']))
-                st.text("Kode Negara : {}".format(negara['alpha-3']))
-                st.text("Region dan Sub Region : {}, {} \n".format(negara['region'], negara['sub-region']))
+                # st.text("Nama Negara : {}".format(negara['name']))
+                # st.text("Kode Negara : {}".format(negara['alpha-3']))
+                # st.text("Region dan Sub Region : {}, {} \n".format(negara['region'], negara['sub-region']))
 
-                st.text("======================================================================================")
-                st.text("Produksi Terkecil : {}".format(chart_data.min()))
-                st.text("Produksi Terkecil Keseruhan : ")
-                sort_pertum2 = chart_pertum.sort_values(by=[negara['alpha-3']], ascending=True)
+                # st.text("======================================================================================")
+                # st.text("Produksi Terkecil : {}".format(chart_data.min()))
+                # st.text("Produksi Terkecil Keseruhan : ")
+                # sort_pertum2 = chart_pertum.sort_values(by=[negara['alpha-3']], ascending=True)
 
-                st.line_chart(sort_pertum2)
-                sort_pertum2
+                # st.line_chart(sort_pertum2)
+                # sort_pertum2
+                df_year = dm.loc[dm['tahun'] == in_tahun]
+                df_year.sort_values(by=['produksi'], ascending=False, inplace=True)
+                df_year.reset_index(drop=True, inplace=True)
+                df2 = dm
+                df2.sort_values(by=['produksi'], ascending=False, inplace=True)
+                df2.reset_index(drop=True, inplace=True)
+                if st.button('Lihat info 1'):
+                    if(len(df_year) > 0) :
+                        max_nat = df_year.head(1)
+                        max_nat_code = str(max_nat["kode_negara"][0])
+                        nat_info = df_js.loc[df_js['alpha-3'] == max_nat_code]
+                        nat_info.reset_index(drop=True, inplace=True)
+                        st.write("Informasi negara dengan jumlah produksi terbesar pada tahun", str(in_tahun))
+                        if(len(nat_info) > 0) :
+                            res = [[str(nat_info['name'][0]), str(max_nat_code), str(nat_info['region'][0]), str(nat_info['sub-region'][0])]]
+                            res = pd.DataFrame(res, columns=['Nama negara', 'Kode negara', 'Region ', 'Sub-region'])
+                            res
+                        else :
+                            st.write("Informasi negara kurang lengkap selain kode negara")
+                            st.write("Kode negara : ", str(max_nat_code))
+                        max_nat_all_year = df2.head(1)
+                        max_nat_all_year_code = str(max_nat_all_year["kode_negara"][0])
+                        nat_all_year_info = df_js.loc[df_js['alpha-3'] == max_nat_all_year_code]
+                        nat_all_year_info.reset_index(drop=True, inplace=True)
+                        st.write("Informasi negara dengan jumlah produksi terbesar keseluruhan tahun")
+                        if(len(nat_all_year_info) > 0) :
+                            res2 = [[str(nat_all_year_info['name'][0]), str(max_nat_all_year_code), str(nat_all_year_info['region'][0]), str(nat_all_year_info['sub-region'][0])]]
+                            res2 = pd.DataFrame(res2, columns=['Nama negara', 'Kode negara', 'Region ', 'Sub-region'])
+                            res2
+                        else :
+                            st.write("Informasi negara kurang lengkap selain kode negara")
+                            st.write("Kode negara : ", str(max_nat_all_year_code))
+                if st.button('Lihat info 2'):
+                    df2 = dm.loc[dm['produksi'] > 0]
+                    df2.sort_values(by=['produksi'], inplace=True)
+                    df2.reset_index(drop=True, inplace=True)
+                    df_year = df2.loc[df2['tahun'] == in_tahun]
+                    df_year.sort_values(by=['produksi'], inplace=True)
+                    df_year.reset_index(drop=True, inplace=True)
+                    if(len(df_year) > 0) :
+                        min_nat = df_year.head(1)
+                        min_nat_code = str(min_nat["kode_negara"][0])
+                        nat_info = df_js.loc[df_js['alpha-3'] == min_nat_code]
+                        nat_info.reset_index(drop=True, inplace=True)
+                        st.write("Informasi negara dengan jumlah produksi terkecil pada tahun", str(in_tahun))
+                        if(len(nat_info) > 0) :
+                            res = [[str(nat_info['name'][0]), str(min_nat_code), str(nat_info['region'][0]), str(nat_info['sub-region'][0])]]
+                            res = pd.DataFrame(res, columns=['Nama negara', 'Kode negara', 'Region ', 'Sub-region'])
+                            res
+                        else :
+                            st.write("Informasi negara kurang lengkap selain kode negara")
+                            st.write("Kode negara : ", str(min_nat_code))
+                        min_nat_all_year = df2.head(1)
+                        min_nat_all_year_code = str(min_nat_all_year["kode_negara"][0])
+                        nat_all_year_info = df_js.loc[df_js['alpha-3'] == min_nat_all_year_code]
+                        nat_all_year_info.reset_index(drop=True, inplace=True)
+                        st.write("Informasi negara dengan jumlah produksi terkecil keseluruhan tahun")
+                        if(len(nat_all_year_info) > 0) :
+                            res2 = [[str(nat_all_year_info['name'][0]), str(min_nat_all_year_code), str(nat_all_year_info['region'][0]), str(nat_all_year_info['sub-region'][0])]]
+                            res2 = pd.DataFrame(res2, columns=['Nama negara', 'Kode negara', 'Region ', 'Sub-region'])
+                            res2
+                        else :
+                            st.write("Informasi negara kurang lengkap selain kode negara")
+                            st.write("Kode negara : ", str(min_nat_all_year_code))
+                if st.button('Lihat info 3'):
+                    df2 = dm.loc[dm['produksi'] == 0]
+                    df2.sort_values(by=['kode_negara'], inplace=True)
+                    df2.reset_index(drop=True, inplace=True)
+                    df2_uniq = df2.kode_negara.unique()
+                    df_year = df2.loc[df2['tahun'] == in_tahun]
+                    df_year.sort_values(by=['kode_negara'], inplace=True)
+                    df_year.reset_index(drop=True, inplace=True)
+                    df_year_uniq = df_year.kode_negara.unique()
+                    if(len(df_year) > 0) :
+                        res = [[0, 0, 0, 0] for i in range(len(df_year_uniq))]
+                        for i in range(len(res)) :
+                            code = str(df_year_uniq[i])
+                            nat_info = df_js.loc[df_js['alpha-3'] == code]
+                            nat_info.reset_index(drop=True, inplace=True)
+                            res[i][0] = str(nat_info['name'][0])
+                            res[i][1] = str(df_year_uniq[i])
+                            res[i][2] = str(nat_info['region'][0])
+                            res[i][3] = str(nat_info['sub-region'][0])
+                        st.write("Informasi negara dengan jumlah produksi nol pada tahun ", str(in_tahun))
+                        res = pd.DataFrame(res, columns=['Nama negara', 'Kode negara', 'Region ', 'Sub-region'])
+                        res
+                        res2 = [[0, 0, 0, 0] for i in range(len(df2_uniq))]
+                        for i in range(len(res2)) :
+                            code = str(df2_uniq[i])
+                            nat_info = df_js.loc[df_js['alpha-3'] == code]
+                            nat_info.reset_index(drop=True, inplace=True)
+                            res2[i][0] = str(nat_info['name'][0])
+                            res2[i][1] = str(df2_uniq[i])
+                            res2[i][2] = str(nat_info['region'][0])
+                            res2[i][3] = str(nat_info['sub-region'][0])
+                        st.write("Informasi negara dengan jumlah produksi nol pada seluruh tahun ")
+                        res2 = pd.DataFrame(res2, columns=['Nama negara', 'Kode negara', 'Region ', 'Sub-region'])
+                        res2
